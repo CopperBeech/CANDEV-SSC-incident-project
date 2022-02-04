d801ffb67c54d0ad0e1271e3660bf252486d5d5a
CopperBeech <jospbarss@gmail.com>
2022-02-04 19:19:20 -0400

    Found statistics for all statuses

--- Status Times.R
+++ Status Times.R
@@ -1,5 +1,7 @@
 incident_history <- read.csv(file = "SSC dataset/INCIDENT_HISTORY.csv", col.names = c("ticket_number", "status", "change_date", "time_in_status_hours"))
 head(incident_history)
+names_of_statuses <- unique(incident_history$status)
+
 incident_history_list <- as.list(split.data.frame(incident_history, incident_history$ticket_number))
 
 find_times <- function(Ticket, Status) {
@@ -7,10 +9,43 @@ find_times <- function(Ticket, Status) {
   if(length(time) == 1){
     return(time)
   } else if (length(time) > 1) {
-    return(time[1])
+    return(sum(time))
   } else return(NA)
 }
 
 queued_times <- sapply(incident_history_list, find_times, Status = "QUEUED")
-head(queued_times)
-mean(queued_times, na.rm = TRUE)
+resolved_times <- sapply(incident_history_list, find_times, Status = "RESOLVED")
+closed_times <- sapply(incident_history_list, find_times, Status = "CLOSED")
+new_times <- sapply(incident_history_list, find_times, Status = "NEW")
+inprog_times <- sapply(incident_history_list, find_times, Status = "INPROG")
+pendingrev_times <- sapply(incident_history_list, find_times, Status = "PENDINGREV")
+pending_times <- sapply(incident_history_list, find_times, Status = "PENDING")
+pendingchg_times <- sapply(incident_history_list, find_times, Status = "PENDINGCHG")
+pendingven_times <- sapply(incident_history_list, find_times, Status = "PENDINGVEN")
+pendingcus_times <- sapply(incident_history_list, find_times, Status = "PENDINGCUS")
+histedit_times <- sapply(incident_history_list, find_times, Status = "HISTEDIT")
+awaitchg_times <- sapply(incident_history_list, find_times, Status = "AWAITCHG")
+awaitcus_times <- sapply(incident_history_list, find_times, Status = "AWAITCUS")
+slahold_times <- sapply(incident_history_list, find_times, Status = "SLAHOLD")
+awaitven_times <- sapply(incident_history_list, find_times, Status = "AWAITVEN")
+
+times_list <- list(queued_times, resolved_times, closed_times, new_times, inprog_times,
+                   pendingrev_times, pending_times, pendingchg_times, pendingven_times,
+                   pendingcus_times, histedit_times, awaitchg_times, awaitcus_times,
+                   slahold_times, awaitven_times)
+
+times_list_na_rm <- sapply(times_list, na.omit)
+
+boxplot(times_list_na_rm)
+mean_times <- sapply(times_list_na_rm, mean)
+median_times <- sapply(times_list_na_rm, median)
+
+#Use log1p to avoid errors from taking the log of 0 values
+log_times_list <- lapply(times_list_na_rm, log1p)
+
+boxplot(log_times_list)
+mean_log_times <- sapply(log_times_list, mean)
+
+output_table <- data.frame(mean_times, mean_log_times, median_times, row.names = names_of_statuses)
+
+print(output_table)
