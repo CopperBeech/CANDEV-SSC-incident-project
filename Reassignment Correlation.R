96152fbc0bc9a7d15e5d817ed856bf072f1b01f3
CopperBeech <jospbarss@gmail.com>
2022-02-04 21:46:59 -0400

    Found correlation

---
+++ Reassignment Correlation.R
@@ -0,0 +1,28 @@
+#Read in the data
+incident_owner_history <- read.csv(file = "SSC dataset/INCIDENT_OWNER_HISTORY.csv",
+                             col.names = c("ticket_number", "status", "assigned_group",
+                                           "parent_service", "service", "change_date",
+                                                                                      "time_in_status_by_owner_hours"))
+head(incident_owner_history)
+##Create a list where each element is a data frame for each ticket
+incident_owner_history_list <- split.data.frame(incident_owner_history, incident_owner_history$ticket_number)
+#This function returns the sum of the time at all statuses
+sum_times <- function(ticket){
+  total_time <- sum(ticket$time_in_status_by_owner_hours, na.rm = TRUE)
+  return(total_time)
+}
+#This function counts the number of groups that worked on a ticket,
+#and then subtracts 1 to give the number of reassignments
+number_of_reassignments <- function(ticket){
+  groups <- ticket$assigned_group
+  groups <- unique(groups)
+  reassignments <- length(groups)-1
+  return(reassignments)
+}
+#Create a vector of incident restoration times
+incident_times <- sapply(incident_owner_history_list, sum_times)
+#Create a vector of incident reassignments
+incident_reassignments <- sapply(incident_owner_history_list, number_of_reassignments)
+#Output a plot and the correlation
+plot(incident_reassignments, incident_times)
+cor.test(incident_reassignments, incident_times, alternative = "two.sided", method = "pearson")
--- Status Times.R
+++ Status Times.R
@@ -1,9 +1,10 @@
 incident_history <- read.csv(file = "SSC dataset/INCIDENT_HISTORY.csv", col.names = c("ticket_number", "status", "change_date", "time_in_status_hours"))
 head(incident_history)
-names_of_statuses <- unique(incident_history$status)
-
+status_names <- unique(incident_history$status)
+##Create a list where each element is a data frame for each ticket
 incident_history_list <- as.list(split.data.frame(incident_history, incident_history$ticket_number))
-
+ 
+#Write a function to find the relevant time for each ticket
 find_times <- function(Ticket, Status) {
   time <- Ticket$time_in_status_hours[Ticket$status == Status]
   if(length(time) == 1){
@@ -13,6 +14,7 @@ find_times <- function(Ticket, Status) {
   } else return(NA)
 }
 
+#Apply the function to the list for each status
 queued_times <- sapply(incident_history_list, find_times, Status = "QUEUED")
 resolved_times <- sapply(incident_history_list, find_times, Status = "RESOLVED")
 closed_times <- sapply(incident_history_list, find_times, Status = "CLOSED")
@@ -28,15 +30,16 @@ awaitchg_times <- sapply(incident_history_list, find_times, Status = "AWAITCHG")
 awaitcus_times <- sapply(incident_history_list, find_times, Status = "AWAITCUS")
 slahold_times <- sapply(incident_history_list, find_times, Status = "SLAHOLD")
 awaitven_times <- sapply(incident_history_list, find_times, Status = "AWAITVEN")
-
+#Put the vectors in a list for simplicity
 times_list <- list(queued_times, resolved_times, closed_times, new_times, inprog_times,
                    pendingrev_times, pending_times, pendingchg_times, pendingven_times,
                    pendingcus_times, histedit_times, awaitchg_times, awaitcus_times,
                    slahold_times, awaitven_times)
-
+#Remove NA values
 times_list_na_rm <- sapply(times_list, na.omit)
-
 boxplot(times_list_na_rm)
+
+#Calculate mean and median times for each status
 mean_times <- sapply(times_list_na_rm, mean)
 median_times <- sapply(times_list_na_rm, median)
 
@@ -44,8 +47,9 @@ median_times <- sapply(times_list_na_rm, median)
 log_times_list <- lapply(times_list_na_rm, log1p)
 
 boxplot(log_times_list)
+#Calculate the mean of the log times
 mean_log_times <- sapply(log_times_list, mean)
-
+#Create a data frame showing statistics for each status
 output_table <- data.frame(mean_times, mean_log_times, median_times, row.names = names_of_statuses)
 
 print(output_table)
