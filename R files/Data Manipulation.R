06463a6602c8c4ef4ddd79562b00d9230afa7750
CopperBeech <jospbarss@gmail.com>
2022-02-06 09:47:08 -0400

    Move code files into folders

--- R files/Data Manipulation.R
+++ R files/Data Manipulation.R
@@ -1,9 +1,11 @@
-incidents <- read.csv("SSC dataset/INCIDENTS.csv", numerals = "allow.loss", col.names = c("ticket_number", 
-                                                                                          "parent_service", "service", "org_id", "assigned_group", 
-                                                                                          "open_date", "close_date", "priority", "status", "actual_completion_hours",
-                                                                                          "business_completion_hours", "aging", "class_structure_id",
-                                                                                          "class_structure", "classification_id", "classification",
-                                                                                          "external_system", "global_ticket_id", "closure_code", "last_modified_date"))
+incidents <- read.csv("SSC dataset/INCIDENTS.csv", numerals = "allow.loss", 
+                      col.names = c("ticket_number", 
+                                    "parent_service", "service", "org_id", "assigned_group", 
+                                    "open_date", "close_date", "priority", "status", "actual_completion_hours",
+                                    "business_completion_hours", "aging", "class_structure_id",
+                                    "class_structure", "classification_id", "classification",
+                                    "external_system", "global_ticket_id", "closure_code", "last_modified_date"))
+
 incident_owner_history <- read.csv(file = "SSC dataset/INCIDENT_OWNER_HISTORY.csv",
                                    col.names = c("ticket_number", "status", "assigned_group",
                                                  "parent_service", "service", "change_date",
